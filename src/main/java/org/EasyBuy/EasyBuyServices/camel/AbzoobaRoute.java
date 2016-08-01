5846ce9bfa55f056c73041fbf10422f54b32d295
Roshan <roshan.atl@gmail.com>
2016-08-01 19:55:37 +0530

    Change Abzooba URL

--- src/main/java/org/EasyBuy/EasyBuyServices/camel/AbzoobaRoute.java
+++ src/main/java/org/EasyBuy/EasyBuyServices/camel/AbzoobaRoute.java
@@ -12,7 +12,7 @@ import org.apache.camel.model.dataformat.JsonLibrary;
 import org.codehaus.jettison.json.JSONObject;
 
 public class AbzoobaRoute extends RouteBuilder {
-	static final String ABZOOBA_REST_URL = "http4://52.23.170.75:5000/model1";
+	static final String ABZOOBA_REST_URL = "http4://52.23.170.75:5000/model2";
 
     @Override
     public void configure() throws Exception {
